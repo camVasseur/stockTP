4d68c1c8431f5d877d03bdbcc211e1547e9e26bf
camVasseur <camille23.vasseur@gmail.com>
2020-09-16 20:17:32 +0200

    interface authentification ok

--- stock/src/Entity/User.php
+++ stock/src/Entity/User.php
@@ -4,11 +4,18 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ *     fields= {"email"},
+ *     message= "l'email que vous avez indiquez est déja utilisé"
+ * )
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -19,6 +26,7 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
      */
     private $email;
 
@@ -29,9 +37,12 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
-     */
+     * @Assert\Length(min="8", minMessage = "Votre mot de passe doit faire minimum 8 caractères" )
+    */
     private $password;
-
+    /**
+     * @Assert\EqualTo(propertyPath= "password", message="Confirmation du mot de passe incorrecte")
+     */
     public $confirm_password;
 
     public function getId(): ?int
@@ -73,5 +84,21 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+        // TODO: Implement getRoles() method.
     }
 }
